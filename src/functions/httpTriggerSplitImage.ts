7d0d2c4f748433eac992c3ba184173d552ba0b05
haiphant <hai.phan@sioux.asia>
2023-11-27 15:37:29 +0700

    implement function logic and reduce configs

--- src/functions/httpTriggerSplitImage.ts
+++ src/functions/httpTriggerSplitImage.ts
@@ -1,11 +1,45 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import mongoose from "mongoose";
+import { databaseConfigs } from "../configs";
+import { ProjectNotFoundError, splitAndUploadImages, updateProject } from "../utils/splitAndUploadImages";
+import { ProjectModel, ProjectStatus } from "../models";
+import { createOrUpdateAKSPod } from "../utils/aks.util";
 
 export async function httpTriggerSplitImage(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+    const projectId = request.query.get('projectId');
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    context.info(`Project ${projectId} start processing split images.`);
 
-    return { body: `Hello, ${name}!` };
+    const body = await request.json() as { fileNames: string[] };
+    const fileNames = body.fileNames;
+
+    mongoose.set('strictQuery', true);
+    mongoose.set('autoCreate', true);
+    await mongoose.connect(databaseConfigs.URI);
+
+    await splitAndUploadImages(projectId, fileNames);
+
+    const project = await ProjectModel.findById(projectId).exec();
+
+    if (!project) {
+        throw new ProjectNotFoundError(projectId);
+    }
+
+    const uploadFirstTime = project.totalImages === 0;
+
+    if (!uploadFirstTime) {
+        await createOrUpdateAKSPod(projectId);
+        console.info('Create AKS successfully!');
+    }
+
+    await updateProject(projectId, {
+        status: ProjectStatus.IN_PROGRESS,
+    });
+    context.info(`Project ${projectId} is ready!`);
+
+    return {
+        body: `Project ${projectId} is ready!`,
+    };
 };
 
 app.http('httpTriggerSplitImage', {
