0ff643650008b257df50b74c438e4387d7b28361
haiphant <hai.phan@sioux.asia>
2023-11-27 15:51:17 +0700

    chore

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -5,5 +5,5 @@ export const appConfigs = {
     Number(process.env.IMAGE_DIMENSIONS_LIMIT) || 5000 * 5000,
   IMAGE_MEMORY_SIZE_LIMIT:
     Number(process.env.IMAGE_MEMORY_SIZE_LIMIT) || 20_000_000, // 20MB
-  UPLOAD_BATCH_SIZE: Number(process.env.UPLOAD_BATCH_SIZE) || 10,
+  UPLOAD_BATCH_SIZE: Number(process.env.UPLOAD_BATCH_SIZE) || 5,
 };
