3798aa7f14836671bc75fbbb005865ec4cdc22f4
haiphant <hai.phan@sioux.asia>
2023-11-27 15:39:28 +0700

    update default configs

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -2,8 +2,8 @@ export const appConfigs = {
   BACKEND_URI: process.env.BACKEND_URL || '',
   IMAGE_SIZE: 256,
   IMAGE_DIMENSIONS_LIMIT:
-    Number(process.env.IMAGE_DIMENSIONS_LIMIT) || 8000 * 6000,
+    Number(process.env.IMAGE_DIMENSIONS_LIMIT) || 5000 * 5000,
   IMAGE_MEMORY_SIZE_LIMIT:
-    Number(process.env.IMAGE_MEMORY_SIZE_LIMIT) || 50_000_000, // 50MB
-  UPLOAD_BATCH_SIZE: Number(process.env.UPLOAD_BATCH_SIZE) || 30,
+    Number(process.env.IMAGE_MEMORY_SIZE_LIMIT) || 20_000_000, // 20MB
+  UPLOAD_BATCH_SIZE: Number(process.env.UPLOAD_BATCH_SIZE) || 10,
 };
